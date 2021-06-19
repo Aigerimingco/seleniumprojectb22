125559db5e68ad82af2fdd9a4caf5aaf29f91c84
Aigerim Ingco <aikish20@yahoo.com>
2021-06-19 17:26:07 +0600

    Driver Util class

--- src/test/java/com/cybertek/tests/day10_webtable_properties_practices/Table_Tasks.java
+++ src/test/java/com/cybertek/tests/day10_webtable_properties_practices/Table_Tasks.java
@@ -1,36 +1,20 @@
 package com.cybertek.tests.day10_webtable_properties_practices;
 
+import com.cybertek.tests.base.TestBase;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.TableUtils;
-import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class Table_Tasks extends TestBase {
 
-public class Table_Tasks {
-    WebDriver driver;
-    @BeforeMethod
-    public void setupMethod(){
-        String browser= ConfigurationReader.getProperty("browser");
-        // this line returns string chrome
-
-        String url= ConfigurationReader.getProperty("dataTableUrl");
-
-        driver = WebDriverFactory.getDriver(browser);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.get(url);
-        // below code same as above
-        //driver.get(ConfigurationReader.getProperty("dataTableUrl"));
-
-    }
     @Test
     public void task3_return_tims_due_amount(){
+
+        String url= ConfigurationReader.getProperty("dataTableUrl");
+        driver.get(url);
        WebElement timsDueAmountCell= driver.findElement(By.xpath("//table[@id='table1']//td[.='Tim']/../td[4]"));
         System.out.println("timsDueAmountCell = " + timsDueAmountCell.getText());
         String actualTimResult = timsDueAmountCell.getText();
@@ -46,6 +30,8 @@ public class Table_Tasks {
 
     @Test
     public void task4_verify_order_method(){
+        String url= ConfigurationReader.getProperty("dataTableUrl");
+        driver.get(url);
 
         TableUtils.verifyOrder(driver,"Tim");
     }
---
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -0,0 +1,62 @@
+package com.cybertek.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    // creating the private cons so this class object
+    //is not reachable from outside
+    private Driver() {
+    }
+
+    /*
+    Making our driver instance private so that its not reachable from outside of the class
+    WQe make it static, cuz we want it to run b4 everything else, also we will use it in a static method
+
+     */
+    private static WebDriver driver;
+
+    /*
+    creating re-usable utility method that will return same driver instance everytime we call it
+     */
+    public static WebDriver getDriver() {
+        if (driver == null) {
+               /*
+               we read our browser type from conf.propertied file using
+               .getProperty method we creating in ConfigurationReader class.
+
+                */
+
+            String browserType = ConfigurationReader.getProperty("browser");
+              /*
+              depending on the browser type our switch statement will determine
+              to open specific type of browser/driver
+               */
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+        }
+        /*
+        same driver instance will be returned every time we c all Driver.gerDriver(); method
+         */
+        return driver;
+
+    }
+
+
+}
